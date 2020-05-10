7796a8fe63413388d3679b48a3d69af5138fc348
TechnikforLife <nico.dichter@gmail.com>
2020-05-10 14:34:46 +0200

    Ueb5 done

--- src/test/test_ueb5.c
+++ src/test/test_ueb5.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+double kappa=0.5;
 void RuKu_3 ( int nDifEqu, /* # der Differentialgleichungen */
               double h, /* Schrittweite */
               double t, /* Kurvenparameter */
@@ -28,6 +29,14 @@ void RuKu_3 ( int nDifEqu, /* # der Differentialgleichungen */
           ( h2 * (k1[i]+k3[i]) + 2 * h * k2[i] ) / 3; }
 
 }
+void generel_osz(int noeq,double t, double* y,double* f){
+	if(noeq!=2){
+		fprintf (stderr,"The amount of equations is not fitting!");
+		exit (EXIT_FAILURE);
+	}
+	f[0]=y[1];
+	f[1]=y[0]-kappa*y[0]*y[0]*y[0];
+}
 void harm_osz(int noeq,double t,double* y,double* f){
 	if(noeq!=2){
 		fprintf (stderr,"The amount of equations is not fitting!");
@@ -42,11 +51,12 @@ int main(){
 	double h,t0,y_0,tend=10,tstep,v_0;  /* Schrittweite, startpunkt, Startwert
                                  Endpunkt, Schritt fuer Ausgabe */ 
 	int noeq=2;                  /* feste Vorgabe der Anzahl der Gleichungen */ 
-	double exact,diff=10;    /* Variablen, um Ergebnis zu speichern und vergleichen*/  
+	//double exact;
+	//double diff=10;    /* Variablen, um Ergebnis zu speichern und vergleichen*/  
 	double *y;         /* Zeiger auf Speicherplaetze, die double enthalten */
 
 	double t,tprint; 
-	double*f,*k1,*k2,*k3;
+	//double*f,*k1,*k2,*k3;
 	/* Eingabe der Parameter */ 
 	printf("Bitte geben Sie h,t0,y_0,v_0,tend und tstep ein: \n");
 	int err=scanf(" %le %le %le %le %le %le",&h,&t0,&y_0,&v_0,&tend,&tstep);  
@@ -55,35 +65,58 @@ int main(){
 	y=mem_alloc(sizeof(double)*noeq);
 	
 	y_check=mem_alloc(sizeof(double)*noeq);     
-	f=mem_alloc(sizeof(double)*noeq); 
-	k1=mem_alloc(sizeof(double)*noeq);   
-	k2=mem_alloc(sizeof(double)*noeq);   
-	k3=mem_alloc(sizeof(double)*noeq);   
+	//f=mem_alloc(sizeof(double)*noeq); 
+	//k1=mem_alloc(sizeof(double)*noeq);   
+	//k2=mem_alloc(sizeof(double)*noeq);   
+	//k3=mem_alloc(sizeof(double)*noeq);   
 	    
 
-	printf("\n   %20s %20s %20s %20s \n","t","exact","RK3","diff");
- 
+	printf("\n   %20s %20s %20s \n","t","RK3","diff");
+	int counter=100;
+	
+	
+	int first=1;
+	//double diffold=0;
+	//printf("%e,%e,%e\n",fabs(t0),fabs(tend),h);
+	FILE* stream2=fopen("data/ueb5_2_d.dat","w");
+	int per=0;
+	double period=0;
+	int issecond_extr=0;
+	double deriv=0;
+	int isfirst=1;
+	int hasfallen=0;
+	double* periods;
+	periods=(double*)mem_alloc (sizeof (double)*counter);
+	for(;y_0<100;y_0+=0.1){
+	
+	first=1;
 	y[0]=y_0;
 	y[1]=v_0;
 	y_check[0]=y_0;
 	y_check[1]=v_0;
 	tprint=t0;
-	int first=1;
-	double diffold=0;
-	double diffnew=1;
-	for(t=t0;t<=tend;t+=h){
-		
-		if(t>=tprint) /* Naechsten Ausgabepunkt erreicht?*/ {
-			exact=y_0*cos(t)+v_0*sin(t);           /* known exact value */ 
-			diff=fabs((exact-y[0]));
+	per=0;
+	period=0;
+	issecond_extr=0;
+	deriv=0;
+	isfirst=1;
+	hasfallen=0;
+	for(t=t0;fabs(t)<=fabs(tend);){
+		if(fabs(t)>=tprint) /* Naechsten Ausgabepunkt erreicht?*/ {
+			//exact=y_0*cos(t)+v_0*sin(t);           /* known exact value */ 
+			//diff=fabs((y_check[0]-y[0]));
 			   /* and rel. error */
-			printf("   %20.5le",t);
-			printf("   %20.5le",exact);
-			printf("   %20.5le",y[0]);
-			printf("   %20.5le",diff);
-			printf("   %20.5le",y_check[0]);
-			printf("   %20.5le\n",fabs(diff-diffold));
-			diffold=diff;
+			//printf("   %20.5le",t);
+			//printf("   %20.5le",y_check[0]);
+			//printf("   %20.5le",y[0]);
+			//printf("   %20.5le\n",diff);
+			//fprintf(stream2,"   %20.5le;",t);
+			//fprintf(stream2,"   %20.5le;",y_check[0]);
+			//fprintf(stream2,"   %20.5le;",y[0]);
+			//fprintf(stream2,"   %20.5le\n",diff);
+			//printf("   %20.5le",fabs((exact-y_check[0])));
+			//printf("   %20.5le\n",fabs(diff-diffold));
+			//diffold=diff;
 			//printf("   %20.5le %20.5le %20.5le %20.5le %20.5le %20.5le\n",
 			//	   t,exact,y[0],diff,y_check[0],fabs((exact-y_check[0])/exact));
 			tprint+=tstep;  /* Ausgabe und naechsten Punkt bestimmen */
@@ -91,11 +124,56 @@ int main(){
  		     
 		/* Dgl.schritt ausfuehren */ 
 		      
-		RK4 (noeq,h,t,y,harm_osz,first );
-		RuKu_3(noeq,h,t,y_check,f,k1,k2,k3,harm_osz);
+		RK4 (noeq,h,t,y,generel_osz,first );
+		//RuKu_3(noeq,h,t,y_check,f,k1,k2,k3,generel_osz);
 		first=0;
+		t+=h;
+		if(per<counter){
+			if(fabs(y[1])<y_0/10){
+				if(isfirst){
+					deriv=fabs(y[1]);
+					isfirst=0;
+					hasfallen=0;
+				}else{
+					if(fabs(y[1])<deriv){
+						deriv=fabs(y[1]);
+						hasfallen=1;
+					}else{
+						if(hasfallen){
+							if(issecond_extr){
+								periods[per]=t-h;
+								per++;
+								issecond_extr=0;
+							}else{
+								issecond_extr=1;
+							}
+							
+							isfirst=1;
+						}else{
+							isfirst=1;
+						}
+					}
+				}
+			}
+			
+		}
 	}  
-	RK4 (noeq,h,t,y,harm_osz,-1 ); 
+	RK4 (noeq,h,t,y,generel_osz,-1 ); 
+	
+	if(per>0){
+		printf("%e,%d\n",y_0,per);
+		period=periods[0]-t0;
+		for(int j=1;j<per;j++){
+			period+=periods[j]-periods[j-1];
+		}
+		period/=per;
+		fprintf(stream2,"%20.5le;%20.5le\n",y_0,period);
+		
+		
+	}
+	}
+			  fclose(stream2);
+	/*
 	t0=0;
 	tend=20;
 	tstep=0.2;
@@ -104,6 +182,7 @@ int main(){
 	char filename[100];
 	void (*fp)(int noeq, double h, double t, double *y,
 		   void (*dgl_rs) (int, double ,double *, double*),int call);
+	fp=RK4;
 	for(int i=0;i<3;i++){
 		if(i==0){
 			y_0=1;
@@ -140,18 +219,18 @@ int main(){
 				y[1]=v_0;
 				tprint=t0;
 				for(t=t0;t<=tend;t+=h){
-					if(t>=tprint) /* Naechsten Ausgabepunkt erreicht?*/ {
-						exact=y_0*cos(t)+v_0*sin(t);           /* known exact value */ 
-						diff=fabs((exact-y[0])/exact);   /* and rel. error */
+					if(t>=tprint)  {
+						exact=y_0*cos(t)+v_0*sin(t);         
+						diff=fabs((exact-y[0])/exact);   
 						fprintf(stream,"%le;",t);
 						fprintf(stream,"%le;",exact);
 						fprintf(stream,"%le;",y[0]);
 						fprintf(stream,"%le\n",diff);
 						//printf("   %20.5le %20.5le %20.5le %20.5le %20.5le %20.5le\n",
 						//	   t,exact,y[0],diff,y_check[0],fabs((exact-y_check[0])/exact));
-						tprint+=tstep;  /* Ausgabe und naechsten Punkt bestimmen */
+						tprint+=tstep;  
 					}
-					/* Dgl.schritt ausfuehren */    
+					   
 					fp (noeq,h,t,y,harm_osz,first );
 					first=0;
 				}
@@ -164,6 +243,6 @@ int main(){
 		}
 		
 		
-	}
+	}*/
 	mem_free_all ();
 }//1e-4 0 1 0 10 1
